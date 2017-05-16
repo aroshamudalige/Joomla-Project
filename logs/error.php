7e6a675d01307be81c548b8ba01b2d83f70ca327
Arosha <aroshamudalige1@gmail.com>
2017-05-16 13:41:52 +0530

    A site using Jommla CMS tool

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-14 11:47:16 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-14T11:47:16+00:00	INFO 61.245.163.145	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-14T15:13:45+00:00	INFO 61.245.165.49	joomlafailure	Username and password do not match or you do not have an account yet.
